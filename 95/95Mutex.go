a6f896576f7449b1bf8d2097f3bb71829ece6046
EduardoMDiniz <edumedeiros.medeiros5@gmail.com>
2024-06-19 09:42:38 -0300

    att 26.1

--- 95/95Mutex.go
+++ 95/95Mutex.go
@@ -1,7 +1,44 @@
 package main 
 
-import(
+import (
 	"fmt"
+	"runtime"
+	"sync"
 )
 
 //Mutex é basicamennte exclusão mutua, ele faz com que apenas uma GoRoutine possa usar aquela variável até finalizar seu uso dela
+
+var wg sync.WaitGroup  //waitgroup
+
+var mu sync.Mutex //criamos uma var mu do tipo sync.mutex e ganhamos 2 métodos com esse tipo, lock e unnlock
+
+func main (){
+
+	contador := 0
+	total_de_GoRoutines := 100
+
+	wg.Add(total_de_GoRoutines) //iniciei o waitgroup
+
+	for i := 0; i < total_de_GoRoutines; i++{ //for com func anonima dentro pra iterar 10x e emular 10 funções
+
+		go func (){
+
+			mu.Lock()        //usar lock no começo da func pra travar as variaveis que for usar 
+
+			v := contador
+			runtime.Gosched() //sleep time
+			v++
+			contador = v
+
+			mu.Unlock()      //usar unlock pra soltar as variaveis da func nan hora que terminar a func
+
+			wg.Done()
+		}()
+			
+	}
+	wg.Wait()
+	fmt.Println(contador)
+	
+}
+
+//resultado = 100 pq dessa vez a integridade da variavel se manteve
--- README.md
+++ README.md
@@ -391,3 +391,19 @@ EP 27
     - tipo ordenarPorLucroProDonoDoPosto
 - Go Playground: https://play.golang.org/p/KOIhAsE3OK
 
+EP 28
+
+- Opção 1: uma pasta, vários arquivos.
+    - package declaration em todos os arquivos
+    - package scope: um elemento de um arquivo é acessível de todos os arquivos
+    - imports tem file scope
+- Opção 2: separando por packages.
+    - pastas diferentes
+    - requer imports
+    - para usar: package.Função()
+- Exportado vs. não-exportado, ou seja, visível vs. não-visível
+    - Em Go não utilizamos os termos "público" e "privado" como em outras linguagens
+    - É somente questão de capitalização
+        - Com maiúscula: exportado, visível fora do package
+        - Com minúscula: não exportado, não utilizável fora do package
+- Artigo: https://rakyll.org/style-packages/
